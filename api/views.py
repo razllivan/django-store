a49223b97e7cae88a4f4ab43204f6f77aacba0c9
Ivan Shanin <shanin-i2011@yandex.ru>
2023-03-28 19:48:41 +0300

    added custom permission for ProductModelViewSet

--- api/views.py
+++ api/views.py
@@ -1,5 +1,6 @@
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
-
+from api.permissions import IsAdminOrReadOnly
 from products.models import Product
 from products.serializers import ProductSerializer
 
@@ -7,3 +8,4 @@ from products.serializers import ProductSerializer
 class ProductModelViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    permission_classes = (IsAdminOrReadOnly,)
