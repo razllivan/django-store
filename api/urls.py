62a892e42909e9114da5c011de9037771e69f7a3
Ivan Shanin <shanin-i2011@yandex.ru>
2023-03-28 23:03:42 +0300

    added obtaining auth token only for admins

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
+from api.views import CustomAuthToken
 from api.views import ProductModelViewSet
 
 app_name = 'api'
@@ -10,4 +11,5 @@ router.register(r'products', ProductModelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('token-auth/', CustomAuthToken.as_view()),
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
+from http import HTTPStatus
+
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.authtoken.models import Token
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.permissions import IsAdminOrReadOnly
@@ -11,3 +16,14 @@ class ProductModelViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAdminOrReadOnly,)
+
+
+class CustomAuthToken(ObtainAuthToken):
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        if not user.is_staff:
+            return Response({'token': 'Получить токен могут только администраторы'}, status=HTTPStatus.FORBIDDEN)
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({'token': token.key})
