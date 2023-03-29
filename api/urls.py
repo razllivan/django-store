9c326dea490851c43cf93014b4d92a1eae6398cc
Ivan Shanin <shanin-i2011@yandex.ru>
2023-03-29 17:10:43 +0300

    created api for GET basket

--- api/urls.py
+++ api/urls.py
@@ -2,12 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from api.views import CustomAuthToken
-from api.views import ProductModelViewSet
+from api.views import ProductModelViewSet, BasketModelViewSet
 
 app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'products', ProductModelViewSet)
+router.register(r'baskets', BasketModelViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
--- api/views.py
+++ api/views.py
@@ -3,12 +3,13 @@ from http import HTTPStatus
 from rest_framework.authentication import TokenAuthentication, SessionAuthentication
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from api.permissions import IsAdminOrReadOnly
-from products.models import Product
-from products.serializers import ProductSerializer
+from products.models import Product, Basket
+from products.serializers import ProductSerializer, BasketSerializer
 
 
 class ProductModelViewSet(ModelViewSet):
@@ -16,6 +17,17 @@ class ProductModelViewSet(ModelViewSet):
     serializer_class = ProductSerializer
     authentication_classes = (TokenAuthentication, SessionAuthentication)
     permission_classes = (IsAdminOrReadOnly,)
+
+
+class BasketModelViewSet(ModelViewSet):
+    queryset = Basket.objects.all()
+    serializer_class = BasketSerializer
+    permission_classes = (IsAuthenticated,)
+    pagination_class = None
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        return queryset.filter(user=self.request.user)
 
 
 class CustomAuthToken(ObtainAuthToken):
